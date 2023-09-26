7195d6b775ac87aadd7343ae511aa4aab1a15137
Oliveira <goliveir@ad.esi.adp.com>
2023-09-26 11:42:09 -0300

    Design done

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -5,23 +5,21 @@ function ToDoList () {
     return (
         <div className="container">
             <h1>To-do List</h1>
-            <form action="post">
-                <div className="inputs-box">
-                    <input className="input-text" type="text" placeholder="Type your task here"/>
-                    <input className="add" type="button" value="Add" />
-                </div>
+            <form className="inputs-box" action="post">
+                <input className="text-input" type="text" placeholder="Type your task here"/>
+                <input className="add-btn" type="button" value="Add" />
             </form>
             <div className="list">
                 <div className="item">
                     <span>Example of task</span>
-                    <button>Delete</button>
+                    <a className="delete">Delete</a>
                 </div>
                 <div className="dashed-item">
                     <span>Example of task</span>
-                    <button>Delete</button>
-                    <button>Undone</button>
+                    <a className="delete">Delete</a>
                 </div>
             </div>
+            <button className="del-btn">Delete All</button>
         </div>
     )
 }
