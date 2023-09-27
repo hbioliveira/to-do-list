30b2c262d98cd8c29000a8112319886788aa354c
Gabriela Oliveira <hbiloliveira@gmail.com>
2023-09-26 21:22:48 -0300

    Added functionalities, but the line through sistem is not working

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,25 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './ToDoList.css';
+import WellDone from './assets/well-done.svg';
+import DoIt from './assets/doit.svg';
 
 function ToDoList () {
+
+    const listStorage = localStorage.getItem('List');
+
+
+    const [newItem, setNewItem] = useState ("");
+    const [list, setList] = useState (listStorage ? JSON.parse(listStorage) : []);
+
+    useEffect(() => {
+        localStorage.setItem('List', JSON.stringify(list));
+    }, [list])
+
+    function addItem(form) {
+        form.preventDefault();
+        if (!newItem) {
+            return;
+        }
+        setList([...list, { text: newItem, isCompleted: false }]);
+        setNewItem("");
+        document.getElementById('entry-input').focus();
+    }
+
+    function clicked(index) {
+        const auxList = [...lista];
+        auxList[index].isCompleted = !auxList[index].isCompleted;
+        setList(auxList);
+    }
+
+    function deleter(index) {
+        const auxList = [...list];
+        auxList.splice(index, 1);
+        setList(auxList);
+    }
+
+    function deleteAll() {
+        setList([]);
+    }
+
     return (
         <div className="container">
             <h1>To-do List</h1>
-            <form className="inputs-box" action="post">
-                <input className="text-input" type="text" placeholder="Type your task here"/>
-                <input className="add-btn" type="button" value="Add" />
+            <form onSubmit={addItem} className="inputs-box">
+                <input 
+                    id="entry-input"
+                    type="text" className="text-input"  
+                    value={newItem} 
+                    onChange={(event) => { setNewItem(event.target.value)}} 
+                    placeholder="Type your task here"
+                />
+                <input className="add-btn" type="submit" value="Add" />
             </form>
             <div className="list">
-                <div className="item">
-                    <span>Example of task</span>
-                    <a className="delete">Delete</a>
-                </div>
-                <div className="line-through-item">
-                    <span>Example of task done</span>
-                    <a className="delete">Delete</a>
-                </div>
+                { 
+                    list.length <1
+                     ? 
+                     <img src={DoIt} className="do-it" /> 
+                     : 
+                     list.map((item, index) => (
+                        <div 
+                            key={index} 
+                            className={item.isCompleted ? "line-through-item" : "item"}
+                        >
+                            <span onClick={() => { clicked(index)}}>{item.text}</span>
+                            <a onClick={() => { deleter(index) }} className="delete">Delete</a>
+                        </div>
+                    ))
+                }
+                {
+                    list.length > 0 &&
+                    <button onClick={() => { deletaTudo() }} className="del-btn">Delete All</button>
+                }
             </div>
-            <button className="del-btn">Delete All</button>
         </div>
     )
 }
